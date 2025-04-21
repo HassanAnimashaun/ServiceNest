4b4bac79ba396e3a2df18f0e65e0c01704303d4e
Hassan Animashaun <hassananimashaun@Hassans-MacBook-Air.local>
2025-04-21 03:23:53 -0400

    created login view for routing connected login page to backend

---
+++ client/src/services/Login.js
@@ -0,0 +1,13 @@
+import Api from '/src/services/Api'
+
+export default {
+  login (credentials){
+    return Api().post('login',credentials)
+  }
+}
+
+
+// AuthenticationService.register({
+//   username: 'admin',
+//   password: 'Admin'
+// })
