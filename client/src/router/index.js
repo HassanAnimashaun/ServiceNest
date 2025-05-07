4cb5899b52c9ef2cb83a62dc617eb05b07e8de50
Hassan Animashaun <hassananimashaun@Hassans-MacBook-Air.local>
2025-05-07 02:03:50 -0400

    update

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,7 +7,7 @@ import HomePage from '@/views/HomePage.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
