9a7618112190ba56e4ec874d87fa8bdb5ffa9a21
Hassan Animashaun <hassananimashaun@MacBookAir.lan>
2025-07-08 07:36:03 -0400

    removed both fetch imports

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const fetch = require("node-fetch");
-const { fetch } = require("undici");
 const { getDb } = require("../db");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
