03e4f7ffe01f04b3487b65f17bdb94511657c62a
Hassan Animashaun <hassananimashaun@MacBookAir.lan>
2025-07-08 08:10:19 -0400

    debugging vite api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 export default () => {
+  const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+  if (!baseURL) {
+    throw new Error('VITE_API_BASE_URL is not defined');
+  }
+
   return axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL,
     withCredentials: true,
   });
 };
-
-if (!import.meta.env.VITE_API_BASE_URL) {
-  throw new Error('VITE_API_BASE_URL is not defined');
-}
