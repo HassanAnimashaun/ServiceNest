756f381919f342f1c6e9ac97745054e5b8e6e988
Hassan Animashaun <hassananimashaun@Hassans-MacBook-Air.local>
2025-07-20 00:21:14 -0400

    fix: appending '/api' to axios baseURL for cleaner request url pt.2

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,11 +2,9 @@ import axios from 'axios';
 
 export default () => {
   let baseURL = import.meta.env.VITE_API_BASE_URL;
-  if (!baseURL.endsWith('/')) baseURL += '/';
-  baseURL += 'api/';
 
   return axios.create({
-    baseURL: baseURL,
+    baseURL: baseURL + '/api',
     withCredentials: true,
   });
 };
