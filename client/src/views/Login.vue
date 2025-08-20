31ecf8e5e4fb6e00478cd4378fae6342a5f864fa
Hassan <130189668+HassanAnimashaun@users.noreply.github.com>
2025-08-20 15:33:18 -0400

    Update Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -125,7 +125,7 @@ export default {
         <button
           type="button"
           @submit="login"
-          class="w-full bg-green-600 text-white rounded-lg py-2 text-sm hover:bg-green-700 transition mb-2"
+          class="login-ctn w-full bg-green-600 text-white rounded-lg py-2 text-sm hover:bg-green-700 transition mb-2"
           :disabled="loading"
         >
           <span v-if="loading">Logging in...</span>
@@ -137,7 +137,7 @@ export default {
 </template>
 
 <style scoped>
-  button[type="button"]{
+  .login-ctn{
     position: relative;
     z-index: 50;
     }
