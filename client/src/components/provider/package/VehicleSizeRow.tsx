6ab0869fe86b83eea26f12f3063380b512199ce8
HassanAnimashaun <hassananimashaun@outlook.com>
2026-07-24 19:25:27 -0400

    feat(provider): add duration toggle state to VehicleSizeRow, hide number input spinners

--- client/src/components/provider/package/VehicleSizeRow.tsx
+++ client/src/components/provider/package/VehicleSizeRow.tsx
@@ -1,4 +1,5 @@
 import type { SizeData, VehicleSize } from '@/config/types'
+import { useState } from 'react'
 
 interface VehicleSizeRowProp {
   size: VehicleSize
@@ -7,35 +8,49 @@ interface VehicleSizeRowProp {
 }
 
 function VehicleSizeRow({ size, data, onChange }: VehicleSizeRowProp) {
-  const [sedanDuration, setSedanDuration] = useState(false)
+  const [duration, setDuration] = useState<null | number>(null)
+
   return (
     <div className="flex flex-col gap-2 border rounded-xl p-2 ">
       <label htmlFor="">{size}</label>
       <label htmlFor="" className="sn-label">
         Duration
       </label>
-      <div className="flex gap-2">
+      <div className="flex gap-4">
         <button
-          className="sn-btn-pill"
-          onClick={() => onChange(size, { duration: 60, price: data.price })}
+          className={`sn-btn-pill ${duration === 60 ? " active" : ""}`}
+          onClick={() => {
+            setDuration(duration === 60 ? null : 60)
+            onChange(size, { duration: 60, price: data.price })
+          }
+          }
         >
           60m
         </button>
         <button
-          className="sn-btn-pill"
-          onClick={() => onChange(size, { duration: 90, price: data.price })}
+          className={`sn-btn-pill ${duration === 90 ? "active" : ""}`}
+          onClick={() => {
+            setDuration(duration === 90 ? null : 90)
+            onChange(size, { duration: 90, price: data.price })
+          }}
         >
           90m
         </button>
         <button
-          className="sn-btn-pill"
-          onClick={() => onChange(size, { duration: 120, price: data.price })}
+          className={`sn-btn-pill ${duration === 120 ? " active" : ""}`}
+          onClick={() => {
+            setDuration(duration === 120 ? null : 120 )
+            onChange(size, { duration: 120, price: data.price })
+          }}
         >
           120m
         </button>
         <button
-          className="sn-btn-pill"
-          onClick={() => onChange(size, { duration: 180, price: data.price })}
+          className={`sn-btn-pill ${duration === 180 ? " active" : ""}`}
+          onClick={() => {
+            setDuration(duration === 180 ? null : 180 )
+            onChange(size, { duration: 180, price: data.price })
+          }}
         >
           180m
         </button>
@@ -43,12 +58,14 @@ function VehicleSizeRow({ size, data, onChange }: VehicleSizeRowProp) {
       <label htmlFor="" className="sn-label">
         Price
       </label>
-      <input
-        type="number"
-        value={data.price ?? ''}
-        onChange={(e) => onChange(size, { duration: data.duration, price: Number(e.target.value) })}
-        className="border rounded h-"
-      />
+        <input
+          type="number"
+          value={data.price}
+          onChange={(e) => {
+            onChange(size, { duration: data.duration, price: Number(e.target.value) })
+          }}
+          className="border rounded"
+        />
     </div>
   )
 }
